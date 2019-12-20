bc7c4e60ef3fb0749ea507852471bbb54125ba2d
Incode <1206751678@qq.com>
2019-12-20 23:13:57 +0800

    https

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 <style lang="scss">
     /*@import 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic';*/
     /*@import url("https://fonts.googleapis.com/css?family=Material+Icons");*/
-    @import url("http://fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic|Material+Icons");
+    @import url("https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic|Material+Icons");
 
     * {
         padding: 0;
