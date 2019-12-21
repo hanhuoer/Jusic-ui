17d532dd0b5e2ce14af497b51eb38442c61a6fae
Incode <1206751678@qq.com>
2019-12-21 17:54:21 +0800

    :wrench: title

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -4,7 +4,7 @@
             <mu-button icon slot="left" @click="isOpenDrawer=!isOpenDrawer">
                 <mu-icon value="menu"></mu-icon>
             </mu-button>
-            Jusic
+            Jusic 点歌台
         </mu-appbar>
         <mu-drawer width="300" :open.sync="isOpenDrawer" :docked="false" :right="false">
             <mu-card style="width: 100%; max-width: 375px; margin: 0 auto;">
