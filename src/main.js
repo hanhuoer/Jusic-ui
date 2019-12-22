151b487a5f32a849db866bb1adc4881fc73dd9fb
Incode <1206751678@qq.com>
2019-12-22 17:00:57 +0800

    :sparkles: add muse-ui-toast

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import MuseUI from 'muse-ui';
 import 'muse-ui/dist/muse-ui.css';
 import theme from 'muse-ui/lib/theme';
 import * as colors from 'muse-ui/lib/theme/colors';
+import Toast from 'muse-ui-toast';
 // import 'typeface-roboto'
 // import './scrollbar'
 
@@ -21,6 +22,17 @@ theme.add('custom-theme', {
 });
 theme.use('custom-theme');
 Vue.use(MuseUI);
+
+Vue.use(Toast, {
+  position: 'top',               // 弹出的位置
+  time: 2000,                       // 显示的时长
+  closeIcon: 'close',               // 关闭的图标
+  close: false,                      // 是否显示关闭按钮
+  successIcon: 'check_circle',      // 成功信息图标
+  infoIcon: 'info',                 // 信息信息图标
+  warningIcon: 'priority_high',     // 提醒信息图标
+  errorIcon: 'warning',              // 错误信息图标
+});
 
 Vue.config.productionTip = false;
 
