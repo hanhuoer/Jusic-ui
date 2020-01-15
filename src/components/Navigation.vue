0e8a37b62e642c439d999cc2b9b6ac5c49188a36
Incode <1206751678@qq.com>
2020-01-15 17:33:12 +0800

    :art: 新增反馈，调整 a 标签跳转新标签

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -23,20 +23,26 @@
             </mu-card>
             <mu-list>
                 <mu-sub-header>社交</mu-sub-header>
-                <mu-list-item button href="https://github.com/hanhuoer">
+                <mu-list-item button href="https://github.com/hanhuoer" target="_blank">
                     <mu-list-item-title>Github</mu-list-item-title>
                 </mu-list-item>
-                <mu-list-item button href="mailto:hanhuoer@126.com">
+                <mu-list-item button href="mailto:hanhuoer@126.com" target="_blank">
                     <mu-list-item-title>邮箱</mu-list-item-title>
                 </mu-list-item>
                 <mu-divider></mu-divider>
                 <mu-sub-header>关于</mu-sub-header>
-                <mu-list-item button href="https://github.com/hanhuoer/Jusic-ui">
+                <mu-list-item button href="https://github.com/hanhuoer/Jusic-ui" target="_blank">
                     <mu-list-item-title>Jusic-ui</mu-list-item-title>
                 </mu-list-item>
-                <mu-list-item button href="https://github.com/hanhuoer/Jusic-serve">
+                <mu-list-item button href="https://github.com/hanhuoer/Jusic-serve" target="_blank">
                     <mu-list-item-title>Jusic-serve</mu-list-item-title>
                 </mu-list-item>
+                <mu-sub-header>反馈</mu-sub-header>
+                <mu-tooltip content="Jusic 点歌台反馈：985590542">
+                    <mu-list-item button href="https://jq.qq.com/?_wv=1027&k=5MBUzwC" target="_blank">
+                        <mu-list-item-title>QQ Group</mu-list-item-title>
+                    </mu-list-item>
+                </mu-tooltip>
             </mu-list>
         </mu-drawer>
     </div>
